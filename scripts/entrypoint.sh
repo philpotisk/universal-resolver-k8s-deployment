0a9248713576e834774220bef1b29f4a72088cb5
Philipp Potisk <philipp.potisk@danubetech.com>
2020-06-09 15:52:04 +0200

    Removed some debug-output

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,19 +6,10 @@ set -e
 
 pwd
 
-base64 --version
-
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-echo "cat /tmp/config"
-cat /tmp/config
-echo "echo KUBECONFIG"
-echo $KUBECONFIG
-
 kubectl version --client --short
-
-echo "kubectl get all --all-namespaces"
 
 kubectl get all --all-namespaces
 
@@ -27,8 +18,6 @@ cp /convert.py /k8s-template.yaml . 2>/dev/null || :
 python --version
 
 python convert.py
-
-ls -al
 
 cd out
 
